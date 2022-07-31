72962b10ba1f9d742854c5d873f94c0ae8b64bd2
HwangHyeJin <h960502hhj@gmail.com>
2022-07-31 21:22:31 +0900

    feat: -메인 화면 개발, - 로그인 화면 개발 - 회원가입 화면 개발 - 비밀번호 갱신화면 개발 - css 적용 - api 개발  - 비밀번호 암호화 함수 개발

--- apps/react-test/src/layouts/main-layout/Index.tsx
+++ apps/react-test/src/layouts/main-layout/Index.tsx
@@ -1,12 +1,29 @@
 import {Outlet} from 'react-router-dom'
+import styled from 'styled-components'
+import {ThemeProvider} from 'styled-components'
+import {theme} from 'styles/theme'
+import Logo from 'assets/images/logo.png'
 
 export const MainLayout: FC = () => {
   return (
-    <>
-      <span>layout</span>
+    <ThemeProvider theme={theme}>
+      <Header>
+        <img src={Logo} />
+      </Header>
       <Outlet />
-    </>
+    </ThemeProvider>
   )
 }
+
+const Header = styled.div`
+  text-align: center;
+  height: 100%;
+
+  img {
+    max-width: 100%;
+    height: auto;
+    margin: 0 auto;
+  }
+`
 
 export default MainLayout
